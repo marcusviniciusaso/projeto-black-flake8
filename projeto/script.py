4311a9c6e3fde23838e24737d98e6f4825641f22
marolive <marolive@redhat.com>
2025-05-07 20:36:01 -0300

    Add script.py

---
+++ projeto/script.py
@@ -0,0 +1,14 @@
+import sys, os
+
+
+def soma(a, b):
+    return a + b
+
+
+def main():
+    resultado = soma(5, 7)
+    print("O resultado é:", resultado)
+
+
+if __name__ == "__main__":
+    main()
